1735ad24f1b849e17bb3265fe1ff2e1e753778bb
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-12-08 00:44:48 +0500

    Introduction of Python
    Python Code

---
+++ Class_03.py
@@ -0,0 +1,20 @@
+# Comment Out Text Code
+# Please Don't Remove This Line Number 03
+# Author : M Fahad Ahmed
+# Course : Python
+"""
+Triple Double Code
+Please Don't Remove This Line Number 03
+Author : M Fahad Ahmed
+Course : Python
+"""
+
+print("Welcome to My Page & Channel Post")
+print("PLZ Subcribe & Follow My Page & Channel Post")
+
+print("Welcome to My Page & Channel Post\n PLZ Subcribe & Follow My Page & Channel Post\n We All Are Team")
+print("All Are Member") # Print This Line
+
+print("hello",6, 9)
+# Print Statement Seprator
+print("hello",6, 9, sep="~")
