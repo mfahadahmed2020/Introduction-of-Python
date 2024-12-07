337aa4b24bd43cb971671c1fd3cab3ae85ecd2cb
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-12-08 00:53:01 +0500

    Introduction of Python
    Python Codes

---
+++ WV&DT_Class_04.py
@@ -0,0 +1,15 @@
+a=19 # (Numeric)
+b="M Fahad Ahmed" # (Text Data)
+c=True # (Boolean True/False)
+d=None
+# list1=[6,7,8][4,-5][Apple,Mango] # (Sequence Data Type Code)
+print(a)
+print(b)
+print(c)
+print(d)
+"""
+Numeric Data Type
+Float 0.9, 45.5
+Integer
+Complex
+"""
