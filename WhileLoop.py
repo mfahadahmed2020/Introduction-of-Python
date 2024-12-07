bee5d62faf27ca086049fdf4975c3c956e072276
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-12-08 01:06:54 +0500

    Introduction of Python
    Python Codes

---
+++ WhileLoop.py
@@ -0,0 +1,23 @@
+# i = 0
+# while(i<4):
+#     print(i)
+#     i = i + 1
+
+# i=int(input("Entre Number: "))
+# while(i<40):
+#     i=int(input("Entre Number: ")) 
+# print(i)
+# print("Done With Loop")
+
+# Decriminted While Loop Ulta Loop
+# count = 9 
+# while(count>0):
+#     print(count)
+#     count= count -1
+
+count = 9 
+while(count>0):
+    print(count)
+    count= count -1
+else:
+    print("Aap Ka Number Complete Hogaya")
